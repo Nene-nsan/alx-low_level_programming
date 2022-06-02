d70ed4cf32c9524015a23329201cbb010fd3788a
Nene-nsan <nsannene@gmail.com>
2022-06-02 15:16:24 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-
-write(STDOUT_FILENO,  err, 59);
+write(STDOUT_FILENO, err, 59);
 return (1);
 }
