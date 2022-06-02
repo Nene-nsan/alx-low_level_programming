318db37d59bbbec14cdb1689732fc5df6e832cce
Nene-nsan <nsannene@gmail.com>
2022-06-02 15:46:26 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Description: prints a string to the standard error
  *
- * Return: Always 1(Error)
+ * Return: 1
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
 write(1, ch, 59);
 return (1);
 }
